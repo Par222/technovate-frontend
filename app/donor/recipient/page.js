cdf957edcdd2071f37ac7556cd67e6ade7e972f0
Kartik Menon <kartikmenon.2002@gmail.com>
2023-09-17 08:21:37 +0530

    donor complete

--- app/donor/recipient/page.js
+++ app/donor/recipient/page.js
@@ -10,23 +10,27 @@ const ReactPlayer = dynamic(() => import("react-player"), { ssr: false });
 const DocViewer = dynamic(() => import("react-doc-viewer"), { ssr: false });
 export default function Chat(){
 	const bloodResult = "https://firebasestorage.googleapis.com/v0/b/miniproject-testpal.appspot.com/o/blood_report%2F100910424_100065099.pdf?alt=media&token=d7124aa0-b6d8-496a-9630-1f9690bcca5f"
-	const recipient = {
-		name: "Sarah Johnson",
-		age: 35,
-		gender: "Female",
-		height: 165,
-		weight: 70,
-		bloodGroup: "A+",
-		organ: "Kidney",
-		healthHistory: [],
-		img: "https://images.pexels.com/photos/3763188/pexels-photo-3763188.jpeg?cs=srgb&dl=pexels-andrea-piacquadio-3763188.jpg&fm=jpg",
-	}
+	// const recipient = {
+	// 	name: "Sarah Johnson",
+	// 	age: 35,
+	// 	gender: "Female",
+	// 	height: 165,
+	// 	weight: 70,
+	// 	bloodGroup: "A+",
+	// 	organ: "Kidney",
+	// 	healthHistory: [],
+	// 	img: "https://images.pexels.com/photos/3763188/pexels-photo-3763188.jpeg?cs=srgb&dl=pexels-andrea-piacquadio-3763188.jpg&fm=jpg",
+	// }
+	const [recipient, setRecipient] = useState()
 
 	async function getMatchedParticipant(){
-		const response = await axios.post(
-			"https://technovate-backend.onrender.com/recipient/match"
+		const response = await axios.patch(
+			"https://technovate-backend.onrender.com/recipient/match", {
+				donor_id:localStorage.getItem("user_id")
+			}
 		);
 		console.log(response)
+		setRecipient(response.data.data)
 	}
 
 	useEffect(() => {
@@ -34,7 +38,7 @@ export default function Chat(){
 	}, [])
 
 	const [rendererList, setRendererList] = useState([]);
-
+	let filteredList = []
 	useEffect(() => {
 		import("react-doc-viewer")
 			.then((module) => {
@@ -45,8 +49,17 @@ export default function Chat(){
 			});
 	}, []);
 
-	const [showBot, setShowBot] = useState(false);
+	useEffect(() => {
+		if (recipient){
+			console.log(recipient)
+			filteredList = recipient.filter((r) => {
+				return r.status === 'Approved'
+			})
+		}
+	}, [recipient])
 
+	const [showBot, setShowBot] = useState(false);
+	
     return (
 		<div>
 			<Navbar />
@@ -56,7 +69,10 @@ export default function Chat(){
 					Matched Recipient
 				</div>
 				<div id="content" className="flex flex-row space-x-10 text-lg">
-					<div className="mx-5 shadow-md flex w-[50%] items-center">
+					{recipient && recipient.length>0 ? recipient.filter((r)=>r.status=="Approved").map((r, index) => {
+						console.log(r)
+						return(
+							<div className="mx-5 shadow-md flex w-[50%] items-center">
 						<img
 							className="w-[350px] h-[350px]"
 							src="https://profilemagazine.com/wp-content/uploads/2022/12/Kenneth-Miles-First-Fidelity-Bank-thumbnail.jpg"
@@ -65,7 +81,7 @@ export default function Chat(){
 							<p className="font-medium my-1">
 								Name:{" "}
 								<span className="text-blue-600  ">
-									{"Arun Mehta"}
+									{r.recipientId.fullname}
 								</span>
 							</p>
 							<p className=" font-medium my-1">
@@ -82,17 +98,17 @@ export default function Chat(){
 							</p>
 							<p className=" font-medium my-1">
 								Age:{" "}
-								<span className="text-blue-600  ">{"39"}</span>
+								<span className="text-blue-600  ">{r.recipientId.age}</span>
 							</p>
 							<p className=" font-medium my-1">
 								Organ:{" "}
 								<span className="text-blue-600  ">
-									{"Kidney"}
+									{r.donorId.organ}
 								</span>
 							</p>
 							<p className=" font-medium my-1">
 								Blood Group:{" "}
-								<span className="text-blue-600  ">O+</span>
+								<span className="text-blue-600  ">{r.recipientId.blood_group}</span>
 							</p>
 							<button
 								className="mt-7 bg-blue-600 text-white py-2 px-4 rounded-md my-2  border-2 border-blue-600 font-semibold  hover:bg-white hover:text-blue-600 transition-all duration-200"
@@ -102,29 +118,9 @@ export default function Chat(){
 							</button>
 						</div>
 					</div>
-					<div>
-						<div id="title" className="font-semibold text-2xl">
-							{" "}
-							Blood Report
-						</div>
-						<DocViewer
-							pluginRenderers={rendererList}
-							documents={[{ uri: bloodResult, fileType: "pdf" }]}
-							config={{
-								header: {
-									disableHeader: true,
-									disableFileName: true,
-									retainURLParams: true,
-								},
-								pdfZoom: {
-									defaultZoom: -0.6, // Adjust the value to fit the PDF within the width
-								},
-							}}
-							theme={{
-								disableThemeScrollbar: true,
-							}}
-						/>
-					</div>
+						)
+					}):(<div>No matches found!</div>)}
+					
 				</div>
 			</div>
 		</div>
